8c736d41d8b28c1be790157c10536fdd18ce4d1d
Jonathan H <jhsucoding@gmail.com>
2022-03-03 13:25:51 -0500

    tested login

---
+++ backend/routes/api/session.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+const router = express.Router();
+
+// Log in
+router.post(
+  '/',
+  asyncHandler(async (req, res, next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.login({ credential, password });
+
+    if (!user) {
+      const err = new Error('Login failed');
+      err.status = 401;
+      err.title = 'Login failed';
+      err.errors = ['The provided credentials were invalid.'];
+      return next(err);
+    }
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user
+    });
+  })
+);
+
+// testing login with username on browser console
+// fetch('/api/session', {
+//   method: 'POST',
+//   headers: {
+//     "Content-Type": "application/json",
+//     "XSRF-TOKEN": ``
+//   },
+//   body: JSON.stringify({ credential: 'Demo-lition', password: 'password' })
+// }).then(res => res.json()).then(data => console.log(data));
+
+// testing login with email on browser console
+// fetch('/api/session', {
+//   method: 'POST',
+//   headers: {
+//     "Content-Type": "application/json",
+//     "XSRF-TOKEN": ``
+//   },
+//   body: JSON.stringify({ credential: 'demo@user.io', password: 'password' })
+// }).then(res => res.json()).then(data => console.log(data));
+
+// testing with invalid user credential and password on browser console
+// fetch('/api/session', {
+//   method: 'POST',
+//   headers: {
+//     "Content-Type": "application/json",
+//     "XSRF-TOKEN": ``
+//   },
+//   body: JSON.stringify({ credential: 'Demo-lition', password: 'Hello World!' })
+// }).then(res => res.json()).then(data => console.log(data));
+
+
+module.exports = router;
