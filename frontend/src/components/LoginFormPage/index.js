e800bdfee71493fde75c87fa7b05db08c6d6a38c
Jonathan H <jhsucoding@gmail.com>
2022-03-13 09:25:58 -0400

    favicon

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -3,7 +3,7 @@ import * as sessionActions from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect, Link } from "react-router-dom";
 
-import "./LoginForm.css";
+import "./LoginFormPage.css";
 
 function LoginFormPage() {
   const dispatch = useDispatch();
